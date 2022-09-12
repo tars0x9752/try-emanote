2bd8e42882d212dade69daa315abc5c921a8c6b3
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-09-12 16:18:03 +0900

    baseUrl2

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
             path = ./content;
             pathString = "./content";
             port = 8080;
-            baseUrl = "/try-emanote";
+            baseUrl = "/try-emanote/";
             # prettyUrls = true;
           };
         };
