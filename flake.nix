80ccf0fc9893daac71bedb7757ef6f35d1872f8c
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-09-12 15:07:05 +0900

    port

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
           sites."default" = {
             path = ./content;
             pathString = "./content";
-            # port = 8080;
+            port = 8080;
             # baseUrl = "/mynotes";
             # prettyUrls = true;
           };
